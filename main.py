9e1026f66f1100b51adeb7db9cea449ff9213575
sakthikvcet <sakthisanjay68@gmail.com>
2024-08-19 21:19:59 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pickle
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+model= pickle.load(open('model.pkl','rb'))
+
+#url
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    prediction=model.predict([[float(request.form.get("temperature"))]])
+    prediction=model.predict([[20]])
+
+    output=round(prediction[0],2)
+    print(output)
+    return render_template('index.html', prediction_text=f'Total revenve generated is Rs.{output}/-')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
